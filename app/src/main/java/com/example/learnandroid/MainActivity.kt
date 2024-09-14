46f7003685fb8cfb2c4a4ca2961a1fa5ab2d98ba
YoulonGG <johnyoulong@gmail.com>
2024-09-14 12:15:10 +0700

    Initial commit

--- app/src/main/java/com/example/learnandroid/MainActivity.kt
+++ app/src/main/java/com/example/learnandroid/MainActivity.kt
@@ -1,47 +1,107 @@
 package com.example.learnandroid
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.learnandroid.ui.theme.LearnAndroidTheme
+import com.example.learnandroid.weekOne.DayOne
 
 class MainActivity : ComponentActivity() {
+
+    private val dayOne = DayOne()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            LearnAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Youlong",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+        //Type of Function
+//        dayOne.funWithArgument("John Youlong", 21,4813)
+//        dayOne.funWithoutArgument()
+//        val result = dayOne.funWithDefaultArgument()
+//        Log.e("Test", "$result")
+
+        //Calling Array from Class DayOne
+//        val arr1 = dayOne.array1
+//        Log.e("Array", arr1.toList().toString())
+
+        //Sum Array
+        val arr2 = dayOne.array2
+        var sum = 0
+        for (arr in arr2) {
+            sum += arr
         }
-    }
-}
+//        Log.e("Array", "$sum")
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        val arr3 = dayOne.nestedArray
+//        Log.e("Array", arr3.contentDeepToString())
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LearnAndroidTheme {
-        Greeting("Android")
+         //Calling Empty ArrayList and add value
+        val arrList = dayOne.arrayList1
+        arrList.add("Apple")
+        arrList.add("Orange")
+        arrList.add("Watermelon")
+        arrList.add("Peach")
+        arrList.add(10.toString())
+//        Log.e("arrayList", "$arrList")
+
+        //Immutable list
+        val list1 = dayOne.immuttablelist
+//        Log.e("list","$list1")
+
+        //Mutable List, Add, Modify and Remove
+        val list2 = dayOne.muttablelist
+        list2.add(10)
+        list2[1] = 99
+        list2.removeAt(3)
+//        Log.e("list","$list2")
+
+        //set
+        val set1 = dayOne.setOf
+//        Log.e("list","$set1")
+
+        //Add add and remove
+        val set2 = dayOne.mutableSetOf1
+        set2.add(99)
+        set2.remove(10)
+//        Log.e("list","$set2")
+
+
+        //Map
+        val map1 = dayOne.map2
+//        Log.e("map", "$map1")
+        val map2 = dayOne.map2
+        Log.e("Map", "$map2")
+
+        //MutableMap
+        val map3 = dayOne.mutablemap
+        //add , modify and
+        map3.put("Geography", 60)
+        map3.remove("Math")
+        Log.e("Map", "$map3")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        enableEdgeToEdge()
     }
 }
---
+++ app/src/main/java/com/example/learnandroid/weekOne/DayOne.kt
@@ -0,0 +1,90 @@
+package com.example.learnandroid.weekOne
+import android.util.Log
+
+//Learning variables and loops
+class DayOne {
+
+    private var name = ""
+    private var age = 0
+    private var id = 0
+
+    //Create Function with Argument || side effect
+    fun funWithArgument(newName: String, newAge: Int, newID: Int){
+        name = newName
+        age = newAge
+        id = newID
+        Log.e("Test",
+            """|
+            |Your name is: $name
+            |Your age is: $age
+            |Your ID is: $id""".trimMargin() )
+    }
+
+    // Create function without Argument
+    fun funWithoutArgument() {
+        name = "John Youlong"
+        age = 21
+        id = 4813
+        Log.e("Test" , """
+            Your name is: $name
+            Your age is: $age
+            Your ID is: $id
+        """.trimIndent())
+    }
+
+    // Create Function with default value
+    fun funWithDefaultArgument(
+        firstName: String = "John",
+        lastName: String = "Youlong",
+        email: String = "johnyoulong@gmail.com",
+        phone: Int = 93383965
+        ): Any{
+        return " First Name: $firstName \n " +
+                "Last Name: $lastName \n " +
+                "Email Address: $email \n " +
+                "Phone Number: $phone"
+    }
+
+    fun sum(vararg n: Int): IntArray {
+        return n + n
+    }
+
+    //Array fixed size so we can not add or delete element
+    //Creation of Array
+    var array1 = arrayOf(10,19,25,2.22,"Apple",false,'$')
+    var array2 = arrayOf(1,2,3,45,6,7,8)
+
+    //Nested Array
+    var nestedArray = Array(5) { Array(3) {5} }
+
+    //Empty ArrayList || This can need to add element
+    val arrayList1 = ArrayList<String>()
+    val arrayList2 = ArrayList<Int>()
+
+    //List allow duplicate value
+    //Immutable Lists
+    //listOf can not add or remove
+    val immuttablelist = listOf("Dog","Cat","Cow","Bat","Bat")
+
+    //Mutable Lists
+    //mutablelistof can add and remove and modify
+    val muttablelist = mutableListOf(10,8,12,3,5,5)
+
+    //Set doesn't allow duplicate value
+    //can not add or remove
+    val setOf = setOf("Gray", "Yellow", 5555, "Red")
+
+    //MutableSetOf
+    val mutableSetOf1: MutableSet<Int> = mutableSetOf(1,2,3,4,5,7,8,9,10)
+    val mutableSetOf2 = mutableSetOf(1,2,3,4,5,7,8,9,10)
+
+    //Map
+    val map1 = mapOf("Apple" to 10, "Orange" to 21, "Pineapple" to 30)
+    //Nested Map
+    val map2 = mapOf(
+        "Person1" to mapOf("userName" to "Youlong", "age" to 21, "Address" to "Phnom Penh"),
+        "Person2" to mapOf("userName" to "Vitu", "age" to 21, "Address" to "Battambong")
+    )
+
+    val mutablemap = mutableMapOf("Math" to 50, "Khmer" to 100, "Biology" to 75)
+}
